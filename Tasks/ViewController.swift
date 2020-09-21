e7cf9e10d1a6830e090c098c8178b1d339dbadef
Omer Mohamed Ali <om@anexia.at>
2020-09-21 08:51:54 +0200

    Initial Commit

---
+++ Tasks/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tasks
+//
+//  Created by Omer Mohamed Ali on 21.09.20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
